9e28d37feebf63897b404669792e7e21942b8673
Steve_Zhang <wentaozhang0305@gmail.com>
2020-04-16 15:42:17 -0700

    Updates

---
+++ precache-manifest.cdcb952675d9dccf6ff66e4fb8f60f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1217ad869fff0827a40181437f340470",
+    "url": "/Monsters-Searching/index.html"
+  },
+  {
+    "revision": "deec53f30d865eb32a9c",
+    "url": "/Monsters-Searching/static/css/main.95467202.chunk.css"
+  },
+  {
+    "revision": "d008ef7a3e9f4d4486bc",
+    "url": "/Monsters-Searching/static/js/2.a3f29769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Searching/static/js/2.a3f29769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deec53f30d865eb32a9c",
+    "url": "/Monsters-Searching/static/js/main.cdb02ac6.chunk.js"
+  },
+  {
+    "revision": "aab4298316b78ee546c2",
+    "url": "/Monsters-Searching/static/js/runtime-main.1b61dc2a.js"
+  }
+]);
